f8de4936bb71d33a7a4f5bcb182b8dd6873be0e0
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-08-10 12:23:47 -0400

    initial checkin

---
+++ demo/models/user_posts.sql
@@ -0,0 +1,13 @@
+with UserPosts as (
+    SELECT
+        p.Id,
+        u.id UserId,
+        u.DisplayName,
+        p.CreationDate
+    from dbo.Posts p
+        left join dbo.Users u
+            on u.Id = p.OwnerUserId)
+
+SELECT
+    *
+from UserPosts
